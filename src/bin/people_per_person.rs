d59fb41dc7ed22fc74c4da078d115dd22e7dd527
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-10-25 10:25:41 +0100

    feat(people_per_person): add new CLI to produce a log per person

--- src/bin/people_per_person.rs
+++ src/bin/people_per_person.rs
@@ -1,3 +1,33 @@
+use std::process;
+
+use people::use_cases;
+use people::use_cases::LogWritten;
+use tracing::info;
+
+use people::config;
+use people::log;
+
 fn main() {
-    println!("nothin here yet :P");
+    info!("Loading config...");
+    let config = match config::get_config() {
+        Ok(config) => config,
+        Err(reason) => {
+            eprintln!("ERROR: {}", reason);
+            process::exit(1);
+        }
+    };
+
+    let log = log::read_logs(&config.people_dir);
+    let per_person_logs = use_cases::split_log_per_person(log, &config);
+    for (person, person_log) in per_person_logs {
+        match use_cases::write_person_log(person, person_log, config.get_per_person_dir()) {
+            LogWritten::Written(path) => println!("Report written to {path:#?}"),
+            LogWritten::FailedToWrite(path, reason) => {
+                println!("ERROR: failed to write {path:#?}  --  reason: {reason}")
+            }
+            LogWritten::NothingToDelete(path) => println!("Nothing to delete: {path:#?}"),
+            LogWritten::Deleted(path) => println!("Report deleted: {path:#?}"),
+            LogWritten::FailedToDelete(path, reason) => println!("{path:#?}  --  reason: {reason}"),
+        }
+    }
 }
